cd0c91b1a4b0ae68e2f65182a5396c65ecd5f0a6
Dmytro Tus <dmytrotus@gmail.com>
2019-08-29 23:40:58 +0200

    Channels seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Channel;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserstableSeeder::class);
+        //$this->call(UserstableSeeder::class);
+        $this->call(ChannelsTableSeeder::class);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,8 +78,30 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
+        <main class="py-4 row">
+            <div class="col-md-4">
+                <div class="container">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        Channels
+                        </div>
+
+                        <div class="panel-body">
+                            <ul class="list-group">
+                                @foreach($channels as $channel)
+                                <li class="list-group-item">
+                                    {{ $channel->title }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                @yield('content')
+            </div>
         </main>
     </div>
 </body>
